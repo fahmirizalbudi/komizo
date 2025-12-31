d5f6bd956d834fd9bec8bf4b6e40cb8f8410a4f4
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-12-31 13:21:02 +0700

    chore: increase upload max bytes

--- src/app/admin/comics/[id]/chapters/actions.ts
+++ src/app/admin/comics/[id]/chapters/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { Chapter } from '@/generated/prisma/client'
 import { ChapterCreateInput } from '@/generated/prisma/models'
 import { prisma } from '@/lib/prisma'
 import { mkdir, writeFile } from 'fs/promises'
@@ -37,4 +38,22 @@ export async function createChapterWithPages(chapter: ChapterCreateInput, pages:
   })
 
   revalidatePath(`/admin/comics/${chapter.comic.connect!.id}/chapters`)
+}
+
+export async function getChapterById(id: string): Promise<Chapter | null> {
+  const chapter = await prisma.chapter.findUnique({
+    where: { id },
+  })
+
+  return chapter
+}
+
+export async function deleteChapter(id: string, comicId: string): Promise<void> {
+  if (!id) return
+
+  await prisma.chapter.delete({
+    where: { id },
+  })
+
+  revalidatePath(`/admin/comic/${comicId}/chapters`)
 }
